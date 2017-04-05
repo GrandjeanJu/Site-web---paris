86373e6e67116b3c62f2568521ca4f28dbb23dc0
GrandjeanJu <grandjean.julien@hotmail.fr>
2017-04-05 14:08:05 +0200

    Pré-mise en ligne
    Prochaine étapes :
    - Joindre la page de connexion et inscription + y rajouter des infos et
    conseils
    -Créer des liens sour naviguer de page en page depuis le site

--- historique.php
+++ historique.php
@@ -1,3 +1,6 @@
+<!-- AFFICHAGE ENTREE DES DONNEES POUR CREER UN PARI (lié à historique_post) -->
+
+
 <?php session_start(); ?>
 <!DOCTYPE html>
 <html>
@@ -36,7 +39,11 @@
         <input type="submit" value="Envoyer" />
     </p>
     </form>
- 
+
+
+<!-- AFFICHAGE TABLEAU -->
+
+
     <?php
         // Connexion à la base de données
         try
@@ -48,7 +55,7 @@
                 die('Erreur : '.$e->getMessage());
         }
         // Récupération des messages
-        $sessid=$_SESSION['id'];
+        $sessid=$_SESSION['id']; // sert à query ci dessous mais aussi pour un query plus loin
         $reponse = $bdd->query('SELECT resultat, id, commentaire, sport, côte_W, côte_L, mise, DATE_FORMAT(date_ajout, "%d/%m/%Y") AS date_addi FROM historique WHERE pseudo='.$sessid.' ORDER BY ID DESC');
     ?>
         <table>
@@ -61,13 +68,44 @@
                 <th>Côte L</th>
                 <th>Commentaire</th>
                 <th>Gagné ?</th>
+                <th>Perte ou Gain </th>
              </tr>
         </thead>
 <?php
-     
         // Affichage de chaque message (toutes les données sont protégées par htmlspecialchars)
         while ($donnees = $reponse->fetch())
-        {
+        { //avec le if si dessous on calcul les gain ou perte et modif de la bdd pour gain_perte
+          if (htmlspecialchars($donnees['resultat'])=="oui") 
+          {
+              $gain_perte=htmlspecialchars($donnees['côte_W'])*htmlspecialchars($donnees['mise']);
+          }
+          elseif (htmlspecialchars($donnees['resultat'])=="non") 
+          {
+              $gain_perte=-htmlspecialchars($donnees['mise']);
+          }
+          else
+          {
+              $gain_perte="En attente";
+          }
+          $insertgain_perte = $bdd->prepare("UPDATE historique SET gain_perte = ? WHERE id = ?");
+          $insertgain_perte->execute(array($gain_perte,$donnees['id']));
+
+        //avec le if si dessous on calcul les gain ou perte et modif de la bdd pour gain_perte_chiffre (la diff par rapport a au dessus c'est qu'on remplace en attente par 0 pour ensuite pouvoir calculer la somme total des gain ou des perte)
+          if (htmlspecialchars($donnees['resultat'])=="oui") 
+          {
+              $gain_perte_chiffre=htmlspecialchars($donnees['côte_W'])*htmlspecialchars($donnees['mise']);
+          }
+          elseif (htmlspecialchars($donnees['resultat'])=="non") 
+          {
+              $gain_perte_chiffre=-htmlspecialchars($donnees['mise']);
+          }
+          else
+          {
+              $gain_perte_chiffre=0;
+          }
+          $insertgain_perte_chiffre = $bdd->prepare("UPDATE historique SET gain_perte_chiffre = ? WHERE id = ?");
+          $insertgain_perte_chiffre->execute(array($gain_perte_chiffre,$donnees['id']));
+
           echo
           '<tbody>
            <tr>
@@ -78,14 +116,20 @@
            <td>' . htmlspecialchars($donnees['côte_L']) . '</td>
            <td>' . htmlspecialchars($donnees['commentaire']) . '</td>
            <td>' . htmlspecialchars($donnees['resultat']) . '</td>
+           <td>' . $gain_perte . '</td>
            <td> <a href="modif_pari.php?id='.$donnees['id'].'">Modifier</a> </td>
            </tr>
           </tbody>';
         }
- 
         $reponse->closeCursor();
-    ?>
- 
+?>
+    	   <tr> <td colspan="8"> 
+         <?php
+              $reponse=$bdd->query('SELECT SUM(gain_perte_chiffre) AS gain_perte_total FROM historique WHERE pseudo='.$sessid.'');
+              $userinfo = $reponse->fetch();
+              echo $userinfo['gain_perte_total'];
+    	   ?> 
+    	   </td> </tr>
       </table>
     </body>
 </html>
